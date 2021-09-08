efd05244e36237eeb413b3b62750656a9cc9e1c1
Charles Washington <charles_was_7@hotmail.com>
2021-09-08 08:28:57 -0300

    Autoload dependencies

--- config/boot.rb
+++ config/boot.rb
@@ -1,12 +1,17 @@
 require 'rubygems'
 require 'bundler'
+require 'sinatra/contrib'
+require 'active_support/dependencies'
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 Bundler.require :default, (ENV['RACK_ENV'] || :development).to_sym
 
-require 'sinatra/contrib'
-require 'active_support/dependencies'
+require 'active_support'
+require_relative '../lib/list_directories'
+require_relative '../lib/application_configuration'
 
-ActiveSupport::Dependencies.autoload_paths += [
-  File.expand_path('../models', __dir__)
-]
+ActiveSupport::Dependencies.autoload_paths += ListDirectories.new.call('./app')
+
+ApplicationConfiguration.configure do |config|
+  config.some_config = 'Added in Boot'
+end
