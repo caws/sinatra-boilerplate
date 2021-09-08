ecebbf7157ea665ac40b6cfefc83471b9136b7a9
Charles Washington <charles_was_7@hotmail.com>
2021-09-08 09:28:39 -0300

    It's an api, so let's render JSON by default.

--- lib/application_controller.rb
+++ lib/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+
+  private
+
+  def halt_422(errors:, message: )
+    halt 422, json(
+      {
+        message: message,
+        errors: errors,
+        success: false
+      }
+    )
+  end
 end
