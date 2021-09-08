e083ac0edab383f7970d1a571a0004e2e992c0f0
Charles Washington <charles_was_7@hotmail.com>
2021-09-08 09:10:14 -0300

    Alright, this code is better. See: https://blog.konnor.site/ruby/dynamic_getters_and_setters/

--- lib/application_configuration.rb
+++ lib/application_configuration.rb
@@ -1,7 +1,20 @@
 class ApplicationConfiguration
   class Config
-    def method_missing(config_name, *args, &block)
-      instance_variable_set("@#{config_name.to_s.gsub('=', '')}", args.first)
+    def method_missing(method_name, *args, &block)
+      raise unless method_name.to_s.end_with?("=")
+
+      setter = method_name
+      getter = method_name.to_s.slice(0...-1).to_sym
+      instance_var = "@#{getter}".to_sym
+      define_singleton_method(setter) do |new_val|
+        instance_variable_set(instance_var, new_val)
+      end
+      define_singleton_method(getter) { instance_variable_get(instance_var) }
+
+      value = args[0]
+      send(setter, value)
+    rescue
+      super(method_name, *args, &block)
     end
   end
 
